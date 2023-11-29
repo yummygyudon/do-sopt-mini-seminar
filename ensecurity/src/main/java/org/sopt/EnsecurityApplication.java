9f71e0c1b3edb3d2fe6efd13007b67bc732b19ee
yummygyudon <bang2brew@gmail.com>
2023-11-29 19:51:45 +0900

    CHORE : 각 모듈 세분화 및 기본 Build 설정

---
+++ ensecurity/src/main/java/org/sopt/EnsecurityApplication.java
@@ -0,0 +1,11 @@
+package org.sopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnsecurityApplication.class, args);
+    }
+}
